d66c0b293ffd88262198cfe36ad629a4d0167c5a
erickyvand <erickyvand@gmail.com>
2020-06-29 13:36:23 +0200

    chore(sequelize): add sequelize
    [Starts #173549315]

---
+++ src/migrations/20200629080715-create-user.js
@@ -0,0 +1,37 @@
+export async const up = (queryInterface, Sequelize) => {
+  await queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    role: {
+      type: Sequelize.ENUM('admin', 'moderator'),
+      defaultValue: 'moderator'
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+export async const down = (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('Users');
+}
